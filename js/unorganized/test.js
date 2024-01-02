f5405c86ebce81df321a9a0781ac2840b63a5b0d
segatcm <segatheycallme@proton.me>
2024-01-02 17:32:34 +0100

    4. cas

--- js/unorganized/test.js
+++ js/unorganized/test.js
@@ -1,15 +1,45 @@
-var a = 530;
-var b = ["a", 50, a, false];
-console.log(b);
+let kemal = 150;
+const staKupujes = prompt("unesi ono sto zelis kupiti");
 
-var osoba = {
-  ime: "sergej",
-  prezime: "knezevic",
-  godiste: 2008,
-}
-console.log(osoba);
+const sok = 60;
+const cokolada = 40;
+const cigare = 460;
 
-function log100() {
-  console.log(100);
+switch (staKupujes.toLowerCase()) {
+  case "sok":
+    if (kemal >= sok) {
+      console.log("🍹🍹🍹🍹🍹🍹🍹🍹🍹");
+      kemal -= sok;
+      console.log("na racunu sada imas: ", kemal);
+    } else {
+      console.log("nemas dovoljno para za sok");
+      console.log("na racunu imas: ", kemal);
+    }
+    break;
+
+  case "cokolada":
+    if (kemal >= cokolada) {
+      console.log("🍫🍫🍫🍫🍫🍫🍫🍫🍫");
+      kemal -= cokolada;
+      console.log("na racunu sada imas: ", kemal);
+    } else {
+      console.log("nemas dovoljno para za cokoladu");
+      console.log("na racunu imas: ", kemal);
+    }
+    break;
+
+  case "cigare":
+    if (kemal >= cigare) {
+      console.log("🚬🚬🚬🚬🚬🚬🚬🚬🚬");
+      kemal -= cigare;
+      console.log("na racunu sada imas: ", kemal);
+    } else {
+      console.log("nemas dovoljno para za cigare");
+      console.log("na racunu imas: ", kemal);
+    }
+    break;
+
+  default:
+    console.log("ne mozes kupiti to")
+    break;
 }
-log100();
