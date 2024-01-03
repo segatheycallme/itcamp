dd79e943294fe1beec84a30dc0796e34d17da88f
segatcm <segatheycallme@proton.me>
2024-01-03 18:52:52 +0100

    6. cas

---
+++ js/unorganized/3br.js
@@ -0,0 +1,33 @@
+const br1 = 10;
+const br2 = 13;
+const br3 = 15;
+
+if (br1 % 15 == 0) {
+  console.log("fizzbuzz za", br1);
+} else if (br1 % 3 == 0) {
+  console.log("fizz za", br1);
+} else if (br1 % 5 == 0) {
+  console.log("buzz za", br1);
+} else {
+  console.log(br1);
+}
+
+if (br2 % 15 == 0) {
+  console.log("fizzbuzz za", br2);
+} else if (br2 % 3 == 0) {
+  console.log("fizz za", br2);
+} else if (br2 % 5 == 0) {
+  console.log("buzz za", br2);
+} else {
+  console.log(br2);
+}
+
+if (br3 % 15 == 0) {
+  console.log("fizzbuzz za", br3);
+} else if (br3 % 3 == 0) {
+  console.log("fizz za", br3);
+} else if (br3 % 5 == 0) {
+  console.log("buzz za", br3);
+} else {
+  console.log(br3);
+}
---
+++ js/unorganized/funk.js
@@ -0,0 +1,4 @@
+function add(x, y) { return x + y }
+const a = 5;
+const b = 6;
+console.log(add(a, b));
