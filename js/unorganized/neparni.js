1d6216929300d85f81c4fdd9f208983425918ee9
segatcm <segatheycallme@proton.me>
2024-02-01 17:30:14 +0100

    17. cas

---
+++ js/unorganized/neparni.js
@@ -0,0 +1,3 @@
+const arr = [1, 24, 5, 6, 7, 8, 7, 64, 3, 6, 6];
+let newArr = arr.filter((val) => val & 1);
+console.log(newArr);
---
+++ js/unorganized/sergejbit.js
@@ -0,0 +1,8 @@
+const ljudi = ["Mervan", "Ahmed", "Silvester", "Veljko", "Nerma", "Faris", "Sergejbit"];
+for (let i = 0; i < ljudi.length; i++) {
+  if (ljudi[i] == "Sergejbit") {
+    ljudi[i] = "Sergej";
+  }
+}
+
+console.log(ljudi)
