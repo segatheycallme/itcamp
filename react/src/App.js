dd2c13f6c9daa59ece5770be40236e64551b8766
segatcm <segatheycallme@proton.me>
2024-05-13 20:53:26 +0200

    sabirac

--- react/src/App.js
+++ react/src/App.js
@@ -3,23 +3,31 @@ import { useState } from 'react';
 
 
 function App() {
-  const [state, setState] = useState(1)
-  const klik = () => {
-    setState(state + 1)
-  }
-  const funkcija2 = () => {
-
-    return < div >
-      <h1 onClick={klik}>caoooo</h1>
-      <h2>{state}</h2>
-    </div >
+  const [broj1, setBroj1] = useState(0)
+  const [broj2, setBroj2] = useState(0)
+  const niz = []
+  for (let i = 0; i < 98; i++) {
+    niz.push(i)
   }
 
   return (
     <div className="App">
-      <header className="App-header">
-        {funkcija2()}
-      </header>
+      <div className="inner">
+        <div className='left'>
+          {niz.map((el) => <button onClick={() => setBroj1(el)}>{el}</button>)}
+        </div>
+        <span className="aa"></span>
+        <div className='right'>
+          {niz.map((el) => <button onClick={() => setBroj2(el)}>{el}</button>)}
+        </div>
+      </div>
+      <div className="inner2">
+        <span>{broj1}</span>
+        <span>+</span>
+        <span>{broj2}</span>
+        <span>=</span>
+        <span>{broj1 + broj2}</span>
+      </div>
     </div>
   );
 }
