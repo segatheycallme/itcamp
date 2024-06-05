708b987bbfd77301e14fe11a55be07a3811aa191
segatcm <segatheycallme@proton.me>
2024-06-05 19:57:28 +0200

    vaktija

--- react/src/pages/Cas.js
+++ react/src/pages/Cas.js
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
-import Dugmic from '../components/Dugmic';
-import { Typography } from '@mui/material';
+import { Button, TextField } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 
+export default function Cas() {
+  const [data, setData] = useState({})
+  const [number, setNumber] = useState(77)
+  const [input, setInput] = useState(77)
 
-export default function Cas(props) {
-  const [color, setColor] = useState(["primary", "secondary"])
+  const getSomething = () => {
+    fetch(`https://api.vaktija.ba/vaktija/v1/${number}`)
+      .then(response => response.json())
+      .then(json => {
+        setData(json)
+        return json
+      })
+  }
+  const handleChangeNumber = () => {
+    setNumber(input)
+  }
+
+  useEffect(() => {
+    getSomething()
+  }, [number])
+
   return (
     <div>
-      <Dugmic lastColors={color} setColor={setColor}>zdravoooooooooo</Dugmic>
-      <Typography>dugmic je {color[0]}</Typography>
-      <Typography>username: {props.user.username}</Typography>
-      <Typography>password: {props.user.password}</Typography>
-    </div >
-  );
+      {
+        <span>{data?.lokacija}</span>
+      }
+      {
+        data?.vakat?.map((el) => {
+          return <span>{el}</span>
+        })
+      }
+      <TextField onChange={(event) => { setInput(Number(event.target.value)) }} value={input} type='text' />
+      <Button onClick={getSomething}>Daj mi vreme za namaz...</Button>
+      <Button onClick={handleChangeNumber}>Daj mi drzavau sa id {input}</Button>
+    </div>
+  )
 }
-
--- react/src/pages/Todo.js
+++ react/src/pages/Todo.js
@@ -4,14 +4,14 @@ import React, { useState } from "react";
 export default function Cas() {
   const [buf, setBuf] = useState("");
   const [todos, _] = useState([]);
+  const [render, rerender] = useState(true);
 
-  console.log(buf)
-  console.log("todos:", todos)
   return (
     <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", minHeight: "100vh" }}>
       <Paper elevation={8} sx={{ minWidth: "400px", minHeight: "400px" }}>
         <TextField onChange={(e) => { setBuf(e.target.value) }} value={buf}></TextField>
-        <Button onClick={() => { if (buf.trim() !== "") { todos.push(buf) }; setBuf("") }}>klik</Button>
+        <Button variant="outlined" onClick={() => { if (buf.trim() !== "") { todos.push(buf) }; setBuf("") }}>dodaj</Button>
+        <Button variant="outlined" onClick={() => { todos.pop(); rerender(!render) }}>izbrisi</Button>
         {todos.map((val) => { return (<Typography>{val}</Typography>) })}
       </Paper>
     </Box >
