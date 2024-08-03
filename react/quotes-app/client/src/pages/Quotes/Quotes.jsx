b60f00b684e7b36dacfa9e3a29b067fc72c76cbf
segatcm <segatheycallme@proton.me>
2024-08-04 00:12:12 +0200

    quotes

--- react/quotes-app/client/src/pages/Quotes/Quotes.jsx
+++ react/quotes-app/client/src/pages/Quotes/Quotes.jsx
@@ -1,5 +1,35 @@
+import { useContext, useEffect, useState } from "react"
+import { getQuotes } from "../../utils/api"
+import { userContext } from "../../App"
+import "./Quotes.css"
+import { Card, CardActions, CardContent, Divider, Stack, Typography } from "@mui/joy"
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa'
+
 export default function Quotes() {
+  const { accessToken } = useContext(userContext)
+  const [_quotes, setQuotes] = useState([])
+
+  useEffect(() => {
+    getQuotes(accessToken).then((v) => { setQuotes(v) })
+  }, [])
+
   return (
-    <></>
+    <Stack justifyContent="start" gap="0.5em" alignItems="center" minHeight="calc(100vh - 6em)" pt="1em">
+      <Card variant="outlined" color="primary" sx={{ width: "calc(20% + 35em)", maxWidth: "90%" }}>
+        <CardContent orientation="horizontal">
+          <CardActions orientation="vertical" sx={{ width: "min-content", gap: 0, alignItems: "center", pt: 0 }}>
+            <button className="vote"><FaAngleUp /></button>
+            <Typography fontSize="lg" textAlign="center">50%</Typography>
+            <Typography fontSize="sm" textAlign="center">10/10</Typography>
+            <button className="vote"><FaAngleDown /></button>
+          </CardActions>
+          <Divider orientation="vertical" />
+          <Stack justifyContent="space-between" width="100%">
+            <Typography textAlign="left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam fuga amet fugit soluta laborum error enim voluptatem culpa repudiandae sed eum, ea rem accusamus facere optio beatae a praesentium tenetur.</Typography>
+            <Typography textAlign="right" color="neutral">-avdo mahmutovic</Typography>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Stack>
   )
 }
--- react/quotes-app/client/src/utils/api.js
+++ react/quotes-app/client/src/utils/api.js
@@ -1,10 +1,9 @@
 import ky from "ky";
 
 export async function logIn(username, password) {
-  return ky.post("http://localhost:8000/sessions", { json: { username, password } }).json()
-    .then((json) => {
-      return json.accessToken
-    }).catch(() => {
-      return Promise.reject("401")
-    })
+  return (await ky.post("http://localhost:8000/sessions", { json: { username, password } }).json()).accessToken
+}
+
+export async function getQuotes(token) {
+  return await ky.get("http://localhost:8000/quotes", { headers: { Authorization: "Bearer " + token } }).json()
 }
