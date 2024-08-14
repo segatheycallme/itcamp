4bdd3fe4161f00fa2beedb76da7dd3f185cdc930
segatcm <segatheycallme@proton.me>
2024-08-14 18:20:49 +0200

    add error helper text

--- react/quotes-app/client/src/pages/LogIn/LogIn.jsx
+++ react/quotes-app/client/src/pages/LogIn/LogIn.jsx
@@ -26,6 +26,7 @@ export default function LogIn() {
           }}>
             <Input error={error} value={username} onChange={(event) => { setUsername(event.target.value) }} placeholder="Username" size="lg" required sx={{ marginBottom: "0.6em" }} />
             <Input error={error} value={password} onChange={(event) => { setPassword(event.target.value) }} placeholder="Password" size="lg" required type="password" />
+            {error ? <Typography level="body-md" ml="0.5em" color="danger">Incorrect username or password</Typography> : <></>}
             <CardActions>
               <Button size="lg" type="submit">LOG IN</Button>
             </CardActions>
