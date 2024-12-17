20af001498ec36a45dbabe740ead068957596a6e
segatcm <segatheycallme@proton.me>
2024-12-17 19:52:15 +0100

    sql hw

---
+++ next/sql/hw.sql
@@ -0,0 +1,28 @@
+CREATE TABLE users (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL
+);
+CREATE TABLE profile (
+    profile_id INT PRIMARY KEY,
+    description VARCHAR(1024) NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+CREATE TABLE post (
+    post_id INT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    body VARCHAR(255),
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+CREATE TABLE post_tag (
+    post_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post (post_id),
+    FOREIGN KEY (tag_id) REFERENCES tag (tag_id)
+);
+CREATE TABLE tag (
+    tag_id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
