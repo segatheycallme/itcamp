dcbe0494315063cb72f48362453bfbec51a401e0
segatcm <segatheycallme@proton.me>
2024-12-30 20:56:18 +0100

    remove items code

---
+++ next/users/back/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors())
+
+// Routes
+app.use('/api/users', userRoutes);
+
+// Start server
+const PORT = process.env.APP_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
